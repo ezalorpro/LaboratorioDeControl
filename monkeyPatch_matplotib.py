ac9b339798ba098f534f11008002fa0db25bf4bf
ezalorpro <kleiver615@gmail.com>
2019-06-07 20:13:21 -0400

    mas monkey patches

--- monkeyPatch_matplotib.py
+++ monkeyPatch_matplotib.py
@@ -3,19 +3,98 @@ from pyqtgraph.exporters.Exporter import Exporter
 from pyqtgraph import PlotItem
 from pyqtgraph import functions as fn
 
-def cleanAxes(self, axl):
-    if type(axl) is not list:
-        axl = [axl]
-    for ax in axl:
-        if ax is None:
-            continue
-        for loc, spine in ax.spines.items():
-            if loc in ['left', 'bottom']:
-                pass
-            elif loc in ['right', 'top']:
-                spine.set_color('none')
-                # do not draw the spine
-            else:
-                raise ValueError('Unknown spine location: %s' % loc)
-            # turn off ticks when there is no spine
-            ax.xaxis.set_ticks_position('bottom')
+class MatplotlibExporter(Exporter):
+    Name = "Matplotlib Window"
+    windows = []
+    def __init__(self, item):
+        Exporter.__init__(self, item)
+        
+    def parameters(self):
+        return None
+
+    def cleanAxes(self, axl):
+        if type(axl) is not list:
+            axl = [axl]
+        for ax in axl:
+            if ax is None:
+                continue
+            for loc, spine in ax.spines.items():
+                if loc in ['left', 'bottom']:
+                    pass
+                elif loc in ['right', 'top']:
+                    spine.set_color('none')
+                    # do not draw the spine
+                else:
+                    raise ValueError('Unknown spine location: %s' % loc)
+                # turn off ticks when there is no spine
+                ax.xaxis.set_ticks_position('bottom')
+    
+    def export(self, fileName=None):
+        
+        if isinstance(self.item, PlotItem):
+            mpw = MatplotlibWindow()
+            MatplotlibExporter.windows.append(mpw)
+
+            stdFont = 'Arial'
+            
+            fig = mpw.getFigure()
+            
+            # get labels from the graphic item
+            xlabel = self.item.axes['bottom']['item'].label.toPlainText()
+            ylabel = self.item.axes['left']['item'].label.toPlainText()
+            title = self.item.titleLabel.text
+
+            ax = fig.add_subplot(111, title=title)
+            ax.clear()
+            self.cleanAxes(ax)
+            #ax.grid(True)
+            for item in self.item.curves:
+                x, y = item.getData()
+                opts = item.opts
+                pen = fn.mkPen(opts['pen'])
+                if pen.style() == QtCore.Qt.NoPen:
+                    linestyle = ''
+                else:
+                    linestyle = '-'
+                color = tuple([c/255. for c in fn.colorTuple(pen.color())])
+                symbol = opts['symbol']
+                if symbol == 't':
+                    symbol = '^'
+                symbolPen = fn.mkPen(opts['symbolPen'])
+                symbolBrush = fn.mkBrush(opts['symbolBrush'])
+                markeredgecolor = tuple([c/255. for c in fn.colorTuple(symbolPen.color())])
+                markerfacecolor = tuple([c/255. for c in fn.colorTuple(symbolBrush.color())])
+                markersize = opts['symbolSize']
+                
+                if opts['fillLevel'] is not None and opts['fillBrush'] is not None:
+                    fillBrush = fn.mkBrush(opts['fillBrush'])
+                    fillcolor = tuple([c/255. for c in fn.colorTuple(fillBrush.color())])
+                    ax.fill_between(x=x, y1=y, y2=opts['fillLevel'], facecolor=fillcolor)
+                
+                pl = ax.plot(x, y, marker=symbol, color=color, linewidth=pen.width(), 
+                        linestyle=linestyle, markeredgecolor=markeredgecolor, markerfacecolor=markerfacecolor,
+                        markersize=markersize)
+                xr, yr = self.item.viewRange()
+                ax.set_xbound(*xr)
+                ax.set_ybound(*yr)
+            ax.set_xlabel(xlabel)  # place the labels.
+            ax.set_ylabel(ylabel)
+            ax.grid()
+            mpw.draw()
+        else:
+            raise Exception("Matplotlib export currently only works with plot items")
+
+
+class MatplotlibWindow(QtGui.QMainWindow):
+    def __init__(self):
+        from pyqtgraph.widgets import MatplotlibWidget
+        QtGui.QMainWindow.__init__(self)
+        self.mpl = MatplotlibWidget.MatplotlibWidget()
+        self.setCentralWidget(self.mpl)
+        self.show()
+        
+    def __getattr__(self, attr):
+        return getattr(self.mpl, attr)
+        
+    def closeEvent(self, ev):
+        MatplotlibExporter.windows.remove(self)
