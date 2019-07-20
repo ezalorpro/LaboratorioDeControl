524ed97155ce036e206c4ee878022835b0f8d9ca
ezalorpro <kleiver615@gmail.com>
2019-07-20 10:28:40 -0400

    PID tab funcional

--- Main aplication/handlers/PIDHandler.py
+++ Main aplication/handlers/PIDHandler.py
@@ -5,11 +5,20 @@ def PIDHandler(self):
     
     self.main.tfcalcButton2.clicked.connect(lambda: calcular_PID(self))
     self.main.sscalcButton2.clicked.connect(lambda: calcular_PID(self))
+    
+    self.main.kpHSlider2.valueChanged.connect(lambda: calcular_PID(self))
+    self.main.kiHSlider2.valueChanged.connect(lambda: calcular_PID(self))
+    self.main.kdHSlider2.valueChanged.connect(lambda: calcular_PID(self))
+    
+    self.main.kpCheckBox2.stateChanged.connect(lambda: calcular_PID(self))
+    self.main.kiCheckBox2.stateChanged.connect(lambda: calcular_PID(self))
+    self.main.kdCheckBox2.stateChanged.connect(lambda: calcular_PID(self))
 
     self.main.tfdiscretocheckBox2.stateChanged.connect(lambda: PID_bool_discreto(self))
 
     self.main.tfradioButton2.toggled.connect(lambda: PID_stacked_to_tf(self))
     self.main.ssradioButton2.toggled.connect(lambda: PID_stacked_to_ss(self))
+    
 
 
 def calcular_PID(self):
@@ -35,16 +44,17 @@ def calcular_PID(self):
     if self.main.PIDstackedWidget.currentIndex() == 0:
         num = json.loads(self.main.tfnumEdit2.text())
         dem = json.loads(self.main.tfdemEdit2.text())
-        system_op, T = system_creator_tf(self, num, dem)
+        system_pid, T = system_creator_tf(self, num, dem)
     else:
         A = json.loads(self.main.ssAEdit2.text())
         B = json.loads(self.main.ssBEdit2.text())
         C = json.loads(self.main.ssCEdit2.text())
         D = json.loads(self.main.ssDEdit2.text())
-        system_op, T = system_creator_ss(self, A, B, C, D)
+        system_pid, T = system_creator_ss(self, A, B, C, D)
     
-    
-    print(system_)
+    t1, y1 = rutina_step_plot(self, system_pid, T)
+    update_gain_labels(self)
+    rutina_system_info(self, system_pid, T)
 
 
 def PID_bool_discreto(self):
--- Main aplication/rutinas/rutinas_PID.py
+++ Main aplication/rutinas/rutinas_PID.py
@@ -11,6 +11,24 @@ ctrl.step_info = step_info
 
 def system_creator_tf(self, numerador, denominador):
     system = ctrl.tf(numerador, denominador)
+    
+    if self.main.kpCheckBox2.isChecked():
+        kp = self.main.kpHSlider2.value()/20
+    else:
+        kp = 0
+    if self.main.kiCheckBox2.isChecked():
+        ki = self.main.kiHSlider2.value()/20
+    else:
+        ki = 0
+    if self.main.kdCheckBox2.isChecked():
+        kd = self.main.kdHSlider2.value()/50
+    else:
+        kd = 0
+    
+    pid = ctrl.tf([kd, kp, ki],[1, 0])
+    
+    system = ctrl.feedback(pid*system)
+    
     t, y = ctrl.impulse_response(system)
 
     if self.main.tfdiscretocheckBox2.isChecked():
@@ -31,6 +49,24 @@ def system_creator_tf(self, numerador, denominador):
 
 def system_creator_ss(self, A, B, C, D):
     system = ctrl.ss(A, B, C, D)
+    
+    if self.main.kpCheckBox2.isChecked():
+        kp = self.main.kpHSlider2.value()/20
+    else:
+        kp = 0
+    if self.main.kiCheckBox2.isChecked():
+        ki = self.main.kiHSlider2.value()/20
+    else:
+        ki = 0
+    if self.main.kdCheckBox2.isChecked():
+        kd = self.main.kdHSlider2.value()/50
+    else:
+        kd = 0
+    
+    pid = ctrl.tf([kd, kp, ki],[1, 0])
+    
+    system = ctrl.tf2ss(ctrl.feedback(pid*system))
+    
     t, y = ctrl.impulse_response(system)
 
     if self.main.ssdiscretocheckBox2.isChecked():
@@ -70,3 +106,28 @@ def rutina_step_plot(self, system, T):
     self.main.stepGraphicsView2.canvas.draw()
     self.main.stepGraphicsView2.toolbar.update()
     return t, y
+
+def update_gain_labels(self):
+    self.main.kpValueLabel2.setText(str(self.main.kpHSlider2.value()/20))
+    self.main.kiValueLabel2.setText(str(self.main.kiHSlider2.value()/20))
+    self.main.kdValueLabel2.setText(str(self.main.kdHSlider2.value()/50))
+    
+
+def rutina_system_info(self, system, T):
+    info = ctrl.step_info(system, T)
+
+    Datos = ""
+    
+    Datos += str(system) + "\n"
+    Datos += "----------------------------------------------\n"
+    
+    for k, v in info.items():
+        Datos += f"{k} : {v:5.3f}\n"
+
+    dcgain = ctrl.dcgain(system)
+    Datos += f"Ganancia DC: {real(dcgain):5.3f}\n"
+
+    if self.main.PIDstackedWidget.currentIndex() == 0:
+        self.main.tfdatosTextEdit2.setPlainText(Datos)
+    else:
+        self.main.ssdatosTextEdit2.setPlainText(Datos)
