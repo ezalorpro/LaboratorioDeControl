be645a98832ff4c78a84915ce16fe1eb8493bd5a
ezalorpro <kleiver615@gmail.com>
2019-07-27 20:45:35 -0400

    avanzes generales

--- Main aplication/handlers/FuzzyHandler.py
+++ Main aplication/handlers/FuzzyHandler.py
@@ -10,24 +10,27 @@ def FuzzyHandler(self):
     self.main.fuzzyTabWidget.removeTab(2)
     self.main.fuzzyTabWidget.removeTab(1)
     
-    self.main.crearFuzzyButton.clicked.connect(lambda: crear_tabs(self))
+    self.main.generarFuzzyButton.clicked.connect(lambda: crear_tabs(self))
     
     self.main.inputNumber.currentIndexChanged.connect(lambda: seleccion_entrada(self))
     self.main.inputNombre.editingFinished.connect(lambda: nombre_entrada(self))
-    self.main.inputEtiquetasNum.editingFinished.connect(lambda: numero_de_etiquetas(self))
+    self.main.inputEtiquetasNum.editingFinished.connect(lambda: numero_de_etiquetas_in(self))
+    self.main.inputRange.editingFinished.connect(lambda: rango_in(self))
     
-    
-    
+    self.main.etiquetaNumIn.currentIndexChanged.connect(lambda: seleccion_etiqueta_in(self))
+    self.main.etiquetaNombreIn.editingFinished.connect(lambda: nombre_etiqueta_in(self))
+    self.main.etiquetaMfIn.currentIndexChanged.connect(lambda: seleccion_mf_in(self))
+    self.main.etiquetaDefinicionIn.editingFinished.connect(lambda: definicion_in(self))
+
+
 def crear_tabs(self):
-    
     self.main.inputNumber.blockSignals(True)
     self.InputList = []
-    
+
     self.main.fuzzyTabWidget.addTab(self.EntradasTab, 'Entradas')
     self.main.fuzzyTabWidget.addTab(self.SalidasTab, 'Salidas')
     self.main.fuzzyTabWidget.addTab(self.ReglasTab, 'Reglas')
     self.main.fuzzyTabWidget.addTab(self.PruebaTab, 'Prueba')
-    
     NumeroEntradas = int(self.main.estrucNumberInputs.currentText())
     NumeroSalidas = int(self.main.estrucNumberOutputs.currentText())
     
@@ -42,77 +45,102 @@ def crear_tabs(self):
             self.InputList[i]['etiquetas'][j] = inputEtiquetasDic_creator(self, j)
             
     self.main.inputNumber.blockSignals(False)
+    seleccion_entrada(self)
 
-def inputEtiquetasDic_creator(self, j):
-    
-    etiquetaDic = {
-        'nombre': 'etiqueta' + str(j+1),
-        'mf': self.main.etiquetaMf.currentText(),
-        'definicion': json.loads(self.main.etiquetaDefinicion.text()),
-    }
-    
-    return etiquetaDic
-    
-    
+
 def inputDic_creator(self, NumeroEntradas, i):
-    
     inputDic = {
         
         'nombre': 'entrada' + str(i+1),
-        'numeroE': int(self.main.inputEtiquetasNum.text()),
-        'etiquetas': [0]*int(self.main.inputEtiquetasNum.text()),
-        'rango': json.loads(self.main.inputRange.text()),        
+        'numeroE': 3,
+        'etiquetas': [0]*3,
+        'rango': [-10, 10],        
     }
-    
     return inputDic
+
+
+def inputEtiquetasDic_creator(self, j):
+    etiquetaDic = {
+        'nombre': 'etiqueta' + str(j+1),
+        'mf': self.main.etiquetaMfIn.currentText(),
+        'definicion': json.loads(self.main.etiquetaDefinicionIn.text()),
+    }
+    return etiquetaDic
 
     
 def seleccion_entrada(self):
-    numero_input = self.main.inputNumber.currentIndex()
-    self.main.inputNombre.setText(self.InputList[numero_input]['nombre'])
-    self.main.inputEtiquetasNum.setText(str(self.InputList[numero_input]['numeroE']))
-    self.main.inputRange.setText(str(self.InputList[numero_input]['rango']))
+    ni = self.main.inputNumber.currentIndex()
+    self.main.inputNombre.setText(self.InputList[ni]['nombre'])
+    self.main.inputEtiquetasNum.setText(str(self.InputList[ni]['numeroE']))
+    self.main.inputRange.setText(str(self.InputList[ni]['rango']))
+    self.main.etiquetaNumIn.clear()
     
-    self.main.etiquetaNum.clear()
+    for j in range(self.InputList[ni]['numeroE']):
+        self.main.etiquetaNumIn.insertItem(j, str(j+1))
     
-    for j in range(self.InputList[numero_input]['numeroE']):
-        self.main.etiquetaNum.insertItem(j, str(j+1))
-    
-    self.main.etiquetaNombre.setText(self.InputList[numero_input]['etiquetas'][0]['nombre'])
-    self.main.etiquetaMf.setCurrentText(self.InputList[numero_input]['etiquetas'][0]['mf'])
-    self.main.etiquetaDefinicion.setText(str(self.InputList[numero_input]['etiquetas'][0]['definicion']))
+    self.main.etiquetaNombreIn.setText(self.InputList[ni]['etiquetas'][0]['nombre'])
+    self.main.etiquetaMfIn.setCurrentText(self.InputList[ni]['etiquetas'][0]['mf'])
+    self.main.etiquetaDefinicionIn.setText(str(self.InputList[ni]['etiquetas'][0]['definicion']))
     return
 
 
 def nombre_entrada(self):
-    numero_input = self.main.inputNumber.currentIndex()
-    self.InputList[numero_input]['nombre'] = self.main.inputNombre.text()
+    ni = self.main.inputNumber.currentIndex()
+    self.InputList[ni]['nombre'] = self.main.inputNombre.text()
 
-def numero_de_etiquetas(self):
+
+def numero_de_etiquetas_in(self):
+    ni = self.main.inputNumber.currentIndex()
+    ne = int(self.main.inputEtiquetasNum.text())
+    self.InputList[ni]['etiquetas'] = [0]*ne
+    self.InputList[ni]['numeroE'] = ne
+    self.main.etiquetaNumIn.blockSignals(True)
+    self.main.etiquetaNumIn.clear()
     
-    numero_input = self.main.inputNumber.currentIndex()
-    Numero_Etiquetas = int(self.main.inputEtiquetasNum.text())
+    for j in range(self.InputList[ni]['numeroE']):
+        self.main.etiquetaNumIn.insertItem(j, str(j+1))
+        self.InputList[ni]['etiquetas'][j] = inputEtiquetasDic_creator(self, j)
     
-    self.InputList[numero_input]['etiquetas'] = [0]*Numero_Etiquetas
-    self.InputList[numero_input]['numeroE'] = Numero_Etiquetas
+    self.main.etiquetaNumIn.blockSignals(False)
+
+
+def rango_in(self):
+    ni = self.main.inputNumber.currentIndex()
+    self.InputList[ni]['rango'] = json.loads(self.main.inputRange.text())
+
+
+def seleccion_etiqueta_in(self):
+    ni = self.main.inputNumber.currentIndex()
+    ne = self.main.etiquetaNumIn.currentIndex()
     
-    self.main.etiquetaNum.clear()
+    self.main.etiquetaNombreIn.setText(self.InputList[ni]['etiquetas'][ne]['nombre'])
+    self.main.etiquetaMfIn.setCurrentText(self.InputList[ni]['etiquetas'][ne]['mf'])
+    self.main.etiquetaDefinicionIn.setText(str(self.InputList[ni]['etiquetas'][ne]['definicion']))
     
-    for j in range(self.InputList[numero_input]['numeroE']):
-        self.main.etiquetaNum.insertItem(j, str(j+1))
-        self.InputList[numero_input]['etiquetas'][j] = inputEtiquetasDic_creator(self, j)
+
+def nombre_etiqueta_in(self):
+    ni = self.main.inputNumber.currentIndex()
+    ne = self.main.etiquetaNumIn.currentIndex()
+    self.InputList[ni]['etiquetas'][ne]['nombre'] = self.main.etiquetaNombreIn.text()
 
 
+def seleccion_mf_in(self):
+    ni = self.main.inputNumber.currentIndex()
+    ne = self.main.etiquetaNumIn.currentIndex()
+    self.InputList[ni]['etiquetas'][ne]['mf'] = self.main.etiquetaMfIn.currentText()
 
 
-
+def definicion_in(self):
+    ni = self.main.inputNumber.currentIndex()
+    ne = self.main.etiquetaNumIn.currentIndex()
+    self.InputList[ni]['etiquetas'][ne]['definicion'] = json.loads(self.main.etiquetaDefinicionIn.text())
 
 # def etiquetasDic_creator(self, j):
     
 #     etiquetaDic = {
-#         'nombre': self.main.etiquetaNombre.text() + str(j),
-#         'mf': self.main.etiquetaMf.currentText(),
-#         'definicion': json.loads(self.main.etiquetaDefinicion.text()),
+#         'nombre': self.main.etiquetaNombreIn.text() + str(j),
+#         'mf': self.main.etiquetaMfIn.currentText(),
+#         'definicion': json.loads(self.main.etiquetaDefinicionIn.text()),
 #     }
     
 #     return etiquetaDic
